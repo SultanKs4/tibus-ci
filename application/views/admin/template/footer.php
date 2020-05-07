3e7068e1fedf3b7f99a0f9c7e4a0d95a9317e2eb
rizalanhari <rizalanhari6@gmail.com>
2020-05-07 12:37:44 +0700

    feat(view): add admin template

---
+++ application/views/admin/template/footer.php
@@ -0,0 +1,40 @@
+<div class="content">
+    <div class="container-fluid">
+        <div class="section">
+        </div>
+    </div>
+</div>
+<footer class="footer">
+    <div class="container-fluid">
+        <nav>
+            <p class="copyright text-center">
+                ©
+                <script>
+                    document.write(new Date().getFullYear())
+                </script>
+                <a>TiBus Tim</a>, made with love for a better web
+            </p>
+        </nav>
+    </div>
+</footer>
+</div>
+</div>
+</body>
+<!--   Core JS Files   -->
+<script src="<?= base_url() ?>assets/js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
+<script src="<?= base_url() ?>assets/js/core/popper.min.js" type="text/javascript"></script>
+<script src="<?= base_url() ?>assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+<!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+<script src="<?= base_url() ?>assets/js/plugins/bootstrap-switch.js"></script>
+<!--  Google Maps Plugin    -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+<!--  Chartist Plugin  -->
+<script src="<?= base_url() ?>assets/js/plugins/chartist.min.js"></script>
+<!--  Notifications Plugin    -->
+<script src="<?= base_url() ?>assets/js/plugins/bootstrap-notify.js"></script>
+<!-- Control Center for Light Bootstrap Dashboard: scripts for the example pages etc -->
+<script src="<?= base_url() ?>assets/js/light-bootstrap-dashboard.js?v=2.0.0 " type="text/javascript"></script>
+<!-- Light Bootstrap Dashboard DEMO methods, don't include it in your project! -->
+<script src="<?= base_url() ?>assets/js/demo.js"></script>
+
+</html>
