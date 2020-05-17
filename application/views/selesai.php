2d4c1efbc1996dc59b2ad38ed9d8c9276228d9be
falahazmi <azmifa13@gmail.com>
2020-05-18 00:48:45 +0700

    feat(view): Selesai

---
+++ application/views/selesai.php
@@ -0,0 +1,19 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="card1">
+            <div class="card-body">
+                <div class="center">
+                    <div>
+                        <p id="gfg">
+                            <img src="assets2/images/checklist.png" alt="check">
+                        </p>
+                    </div>
+                    <div class="font3">
+                        Selesai
+                    </div>
+                    <div class="button">
+                        <a href="#" class="btn btn-circle btn-success book_btn" data-bus="1">Upload Bukti</a>
+                    </div>
+                </div>
+            </div>
+        </div>
